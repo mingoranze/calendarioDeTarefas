c682cdb2287c5decde4f6eeebb33be20ef99a055
Keven Mingoranze <keven.hm@outlook.com>
2023-02-28 23:07:55 -0300

    sistema funcionando parcialmente, radios ok, input text ok, falta colorir o tarefa item ao selecionar o radio

--- script.js
+++ script.js
@@ -1,22 +1,42 @@
-let closeButton = document.querySelector('.close');
-let modalArea = document.querySelector('.modal-area');
-let tarefaBody = document.querySelector('.tarefa-body');
+const closeButton = document.querySelector('.close');
+const tarefaBody = document.querySelector('.tarefa-body');
+const modalArea = document.querySelector('.modal-area');
+const modalForm = document.querySelector('#form')
+const tarefaItem = document.querySelectorAll('.tarefa-item');
+const tarefaInput = document.querySelector('#text')
+const radios = document.getElementsByName('pessoa');
+
+function close() {
+    modalArea.style.display = 'none'
+}; 
 
 
-closeButton.addEventListener('click', (e)=>{
-    if(modalArea.style.display = 'flex') {
-        modalArea.style.display = 'none'
-    }
+tarefaItem.forEach((day)=>{
+    day.addEventListener('click', ()=>{
+        modalArea.style.display = 'flex';
+        modalForm.onsubmit = (e)=>{
+            e.preventDefault();
+            const value = tarefaInput.value;
+            day.querySelector('.texto').innerHTML = value;
+            for(i=0; i<radios.length; i++) {
+                if(radios[i].checked){
+                    day.querySelector('.pessoa').innerHTML = radios[i].value;
+                } 
+            }
+
+
+            
+            tarefaInput.value = '';
+            close();
+        };
+
+    }); 
 });
 
-tarefaBody.addEventListener('click', (e)=>{
-    modalArea.style.display = "flex";
-    let input = document.querySelector('#text').value;
-    let item = e.target;
-   
-    console.log(item)
-});
 
-function add() {
-   
-}
+
+
+
+
+  
+  
